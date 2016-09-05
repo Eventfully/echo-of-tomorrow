be494b8d5ab7019dbedca5a2885c749dc8347a02
Magnus Palmér <magnus.palmer@enfo.se>
2016-09-05 21:02:17 +0200

    First checkin

---
+++ src/main/groovy/org/eventfully/alexa/eot/EOTSpeechletRequestStreamHandler.groovy
@@ -0,0 +1,32 @@
+package org.eventfully.alexa.eot
+
+import com.amazon.speech.speechlet.Speechlet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+/**
+ * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
+ * experience. To do this, simply set the handler field in the AWS Lambda console to
+ * "session.SessionWorldSpeechletRequestStreamHandler" For this to work, you'll also need to build
+ * this project using the {@code lambda-compile} Ant task and upload the resulting zip file to power
+ * your function.
+ */
+public class EOTSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
+         * Alexa Skill and put the relevant Application Ids in this Set.
+         */
+        supportedApplicationIds = new HashSet<String>();
+        supportedApplicationIds.add("amzn1.ask.skill.1f323533-d614-4649-ab29-78fd62e118eb");
+    }
+
+    public EOTSpeechletRequestStreamHandler() {
+        super(new SessionSpeechlet(), supportedApplicationIds);
+    }
+}
